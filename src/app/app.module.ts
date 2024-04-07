58208a21c47ba33c106bb6a9e84216a053b49140
Harsha vardhan <harsha7595@gmail.com>
2024-04-07 19:35:54 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
-=======
+    AppComponent,HomeComponent,NavComponent,FooterComponent,LoginComponent
 
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> d2274f02e26cfd88c9c8cc556297aa731bb662b7
   ],
   imports: [
     BrowserModule,
